d4811ac5fbf4f8ae6166c1ca5d3463100ba1de47
Rahul <pbrkumawat@gmail.com>
2026-05-28 22:16:48 +0530

    Update Readme.md

---
+++ README.md
@@ -0,0 +1,174 @@
+# Leads Management
+
+An RESET API for leads management, where you can browse,  add, edit & view details of leads.
+Built with  Express/Node backend, Mongodb database.
+
+---
+
+## Demo Link
+
+[Live API](https://github.com/rahulCode1/Anvaya-backend.git)
+
+---
+
+## Quick Start
+
+```
+git clone  https://github.com/rahulCode1/Anvaya-backend.git
+cd backend
+npm install
+npm start
+```
+
+## Technologies
+
+- Node JS
+- Express
+- Mongodb
+
+
+---
+
+## Features
+
+
+- Display list of all leads.
+- Add new lead.
+- Get all sales agent.
+- Add new Sales agent.
+- Manage sales agent & leads.
+
+
+---
+
+## Environment Variables
+```
+PORT=80
+MONGODB=XXXXXXXXXXXXXXXXXXX
+```
+
+---
+
+
+
+## API Reference
+
+### **GET api/leads**
+
+List of all leads
+Sample Response
+
+```
+[{id, name, source, ...}, ...]
+```
+
+### **POST api/lead**<br>
+
+Add new Lead
+Sample Response
+
+```
+{id, name, source...}
+```
+
+### **GET api/lead/id**<br>
+
+Lead details
+Sample Response
+
+```
+{id, name, source...}
+```
+
+### **PATCH api/lead/id**<br>
+
+Edit lead
+Sample Response
+
+```
+{id, name, source...}
+```
+
+### **DELETE api/lead/id**<br>
+
+Delete lead
+
+### **POST api/lead/id/comments**<br>
+
+Add comment on lead
+Sample Response
+
+```
+{id, author, commentText, ...}
+```
+
+### **GET api/lead/id/comments**<br>
+
+Get all comments
+Sample Response
+
+```
+[{id, author, commentText, ...}, ...]
+```
+
+### **GET api/report/last-week**<br>
+
+Get leads closed last week
+Sample Response
+
+```
+{id, name, salesAgent, ...}
+```
+
+### **GET api/report/pipeline**<br>
+
+Get active or closed lead.
+Sample Response
+
+```
+{activeLeads, closedLeads}
+```
+
+### **GET api/report/closed-by-agent**<br>
+
+Get lead closed by agent.
+Sample Response
+
+```
+[
+    {
+        agent: [
+            {
+                id, name, status, ...
+            }
+        ]
+    }
+]
+```
+
+## **POST api/agent**<br>
+
+Add new sales agent
+Sample response
+
+```
+{name, email}
+```
+
+## **GET api/agent**<br>
+
+Get all sales agent
+Sample response
+
+```
+[{name, email}, ...]
+```
+
+## **DELETE api/agent/id**<br>
+
+Delete sales agent
+Sample response
+
+## Contact
+
+For bugs or feature requests, please reach out to rahulkumawat50665@gmail.com
--- index.js
+++ index.js
@@ -1,58 +1,45 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const HttpError = require("./models/http-error");
-const leadsRoutes = require("./routes/lead-routs");
-const { initializeDb } = require("./data/db.connect");
-const errorHandler = require("./middlewares/errorHandler");
-const salesAgentRoute = require("./routes/salesAgent-route");
-const reportRoute = require("./routes/report-route");
-const tagRoute = require("./routes/tag-route");
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const HttpError = require("./models/http-error")
+const leadsRoutes = require("./routes/lead-routs")
+const { initializeDb } = require("./data/db.connect")
+const errorHandler = require("./middlewares/errorHandler")
+const salesAgentRoute = require("./routes/salesAgent-route")
+const reportRoute = require("./routes/report-route")
+const tagRoute = require("./routes/tag-route")
 
-initializeDb();
+initializeDb()
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200
+}
 
-app.use(cors());
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://major-project2-backend-sand.vercel.app/api",
-];
+app.use(cors(corsOptions))
+app.use(express.json())
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE OPTIONS",
-  );
-
-  next();
-});
-
-app.use(express.json());
-
-app.use("/api/leads", leadsRoutes);
-app.use("/api/agents", salesAgentRoute);
-app.use("/api/report", reportRoute);
-app.use("/api/tags", tagRoute);
+app.use("/api/leads", leadsRoutes)
+app.use("/api/agents", salesAgentRoute)
+app.use("/api/report", reportRoute)
+app.use("/api/tags", tagRoute)
 
 app.use((req, res, next) => {
-  next(new HttpError("This route doesn't exist.", 404));
-});
+    next(new HttpError("This route doesn't exist.", 404))
+})
 
-app.use(errorHandler);
 
-const PORT = process.env.PORT;
+
+
+
+
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+    console.log(`Server running on ${PORT}`)
+})
